e634df60246fdbe8927599471be78b048cef8ffe
Tomás Bran <tomasaugusto.bran@hotmail.com>
2024-05-01 01:25:02 +0200

    ci localstorage 3

--- src/app/utils/services/localStorage.js
+++ src/app/utils/services/localStorage.js
@@ -1,19 +1,32 @@
 export const saveToLocalStorage = (key, data) => {
-	try {
-		const jsonData = JSON.stringify(data);
-		localStorage.setItem(key, jsonData);
-	} catch (error) {
-		console.error('Error al guardar en localStorage:', error);
+	if (typeof window !== 'undefined') {
+		try {
+			const jsonData = JSON.stringify(data);
+			localStorage.setItem(key, jsonData);
+		} catch (error) {
+			console.error('Error al guardar en localStorage:', error);
+		}
+	} else {
+		console.error(
+			'No se puede acceder a localStorage porque no estamos en un entorno de navegador.'
+		);
 	}
 };
 
 export const getFromLocalStorage = (key) => {
-	try {
-		const jsonData = localStorage.getItem(key);
-		const data = jsonData ? JSON.parse(jsonData) : null;
-		return data;
-	} catch (error) {
-		console.error('Error al obtener de localStorage:', error);
+	if (typeof window !== 'undefined') {
+		try {
+			const jsonData = localStorage.getItem(key);
+			const data = jsonData ? JSON.parse(jsonData) : null;
+			return data;
+		} catch (error) {
+			console.error('Error al obtener de localStorage:', error);
+			return null;
+		}
+	} else {
+		console.error(
+			'No se puede acceder a localStorage porque no estamos en un entorno de navegador.'
+		);
 		return null;
 	}
 };
