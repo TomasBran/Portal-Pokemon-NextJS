0f616f956cf780ce0abf3c672a0be59b5c3fae13
Tomás Bran <tomasaugusto.bran@hotmail.com>
2024-05-01 21:32:12 +0200

    ci companion

--- src/app/Components/BuildPokemon/Companion.js
+++ src/app/Components/BuildPokemon/Companion.js
@@ -4,13 +4,11 @@ import CompanionArray from './CompanionArray';
 import { useEffect, useState } from 'react';
 
 const Companion = ({ score, stats }) => {
-	const [playScore, setPlayScore] = useState(score);
 	const [companionImage, setCompanionImage] = useState(CompanionArray[0].url);
 
 	useEffect(() => {
 		const newUrl = getRandomUrlByType(score, companionImage);
 		setCompanionImage(newUrl);
-		setPlayScore(score);
 	}, [score, stats]);
 
 	const getRandomUrlByType = (type, currentUrl) => {
@@ -27,43 +25,9 @@ const Companion = ({ score, stats }) => {
 		return filteredCompanions[randomIndex].url;
 	};
 
-	const getScoreSign = (score) => {
-		switch (score) {
-			case 'good':
-			case 'bad':
-				return '❔';
-			case 'best':
-				return '✔️';
-			case 'worst':
-				return '❌';
-			default:
-				return '';
-		}
-	};
-
-	const getScoreClassName = (score) => {
-		switch (score) {
-			case 'good':
-			case 'bad':
-				return 'bg-white/80';
-			case 'best':
-				return 'bg-green-600/80';
-			case 'worst':
-				return 'bg-black/80';
-			default:
-				return '';
-		}
-	};
-
 	return (
 		<div>
 			<div className='bg-white h-full w-full rounded-lg overflow-hidden'>
-				<span
-					className={`${getScoreClassName(
-						playScore
-					)} absolute right-0 bottom-0 m-2 p-1 rounded-full`}>
-					{getScoreSign(playScore)}
-				</span>
 				<Image
 					alt='pikachu'
 					className='h-full w-full'
